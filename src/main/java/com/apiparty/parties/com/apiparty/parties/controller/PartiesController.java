45205e3430ee8875d697819ee9ace41c1146a3b1
minishtera <minisht@gmail.com>
2018-04-16 22:11:44 -0500

    Development by 04/16/2018.

--- src/main/java/com/apiparty/parties/com/apiparty/parties/controller/PartiesController.java
+++ src/main/java/com/apiparty/parties/com/apiparty/parties/controller/PartiesController.java
@@ -1,6 +1,8 @@
 package com.apiparty.parties.com.apiparty.parties.controller;
 
 import com.apiparty.parties.com.apiparty.parties.entity.Parties;
+import com.apiparty.parties.com.apiparty.parties.service.PartiesSrvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PartiesController {
+
+    @Autowired
+    PartiesSrvc partiesSrvc;
+
     @GetMapping(path = "/parties/{id}")
     public Parties getParty(@PathVariable(name="id") Integer id) {
-        return null;
+        return partiesSrvc.getParty(id);
     }
+
+
 
 
 }
